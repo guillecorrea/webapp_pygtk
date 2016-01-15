0a9d439594beb9f737c43ba3a642b2f0c663bb82
Guillermo Correa <guillecorrea@gmail.com>
2016-01-15 09:37:11 -0300

    Update server.py
    a

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import BaseHTTPServer, SimpleHTTPServer 
-
+# TODO : agregar comprobacion de user agent y de clave para que solo el visor pueda acceder al servidor . 
 s = BaseHTTPServer.HTTPServer(('', 6566), SimpleHTTPServer.SimpleHTTPRequestHandler)
 s.serve_forever()
